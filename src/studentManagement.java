32b6af737b8ed654aa08fc1ec7fc66729b29acba
youjin9209 <youjin9200@naver,com>
2015-10-29 16:04:37 +0900

    name simplify updated

--- src/studentManagement.java
+++ src/studentManagement.java
@@ -1,34 +1,38 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import java.sql.*;
-
 import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.*;
 
 
-
 public class studentManagement extends JFrame implements ActionListener{
-
-	//db Connect
-	Connection conn;
+	
+	Connection conn; //DB Connection 
 	Statement  stat;
-
 	ResultSet rs = null;
+	
 	JTabbedPane tabpane; //Create the tab
-	Button insertInfo,search,deleteBtn, search2,updateBtn, viewBtn;
-	TextField valID, valID2, valID3, valID4, valName, valDept, valPhone, valNewPhone;
-
-
+	Button insertInfoBtn, deleteBtn, updateBtn, viewBtn;
+	//updatedPhone number
+	TextField updatedPhone;
+	
+	//To insert id,name,department,phone   
+	TextField insertId, insertName, insertDepartment, insertPhone; 
+	
+	//searchToDeleteBtn id to delete,to update,to view
+	TextField searchIdToDelete, searchIdToUpdate, searchIdToView;
+	
+	//search id button to delete, search id button to update  
+	Button searchToDeleteBtn, searchToUpdateBtn;
 	JPanel addStudent;
 
 	String index[] = {"ID","NAME","DEPARTMENT","PHONE"}; //table index create
 	DefaultTableModel listmodel = new DefaultTableModel(index,0); //create table model
 	JTable studentList = new JTable(listmodel); //view table create
 	
-	TextField inform, inform2;
+	TextField informToDelete, informToUpdate;
 
 	public studentManagement(){
 
@@ -40,9 +44,8 @@ public class studentManagement extends JFrame implements ActionListener{
 		JPanel updateStudent = new JPanel();
 		JPanel viewStudent = new JPanel();
 
-		////////////////////////////////////
-
-		//UI ���� ���� ����
+		
+		//UI Component
 		Panel IDpanel, NamePanel, DeptPanel, PhonePanel, IDpanel2, IDpanel3, phonePanel2;
 		Label stID, stName, stDept, stPhone, stID2, stID3,newPhone, stID4;
 
@@ -58,22 +61,23 @@ public class studentManagement extends JFrame implements ActionListener{
 		stDept = new Label("Department");
 		stPhone = new Label("Phone number");
 
-		//insert value here
-		valID = new TextField(20);
-		valName = new TextField(20);
-		valDept = new TextField(20);
-		valPhone = new TextField(20);
+		/*
+		 *  INSERT STUDENT ID,NAME,DEPARTMENT,PHONE IN HERE
+	     */
+		insertId = new TextField(20);
+		insertName = new TextField(20);
+		insertDepartment = new TextField(20);
+		insertPhone = new TextField(20);
 
-		//������ ���۳�Ʈ �߰�
-
+		
 		IDpanel.add(stID);
-		IDpanel.add(valID);
+		IDpanel.add(insertId);
 		NamePanel.add(stName);
-		NamePanel.add(valName);
+		NamePanel.add(insertName);
 		DeptPanel.add(stDept);
-		DeptPanel.add(valDept);
+		DeptPanel.add(insertDepartment);
 		PhonePanel.add(stPhone);
-		PhonePanel.add(valPhone);
+		PhonePanel.add(insertPhone);
 
 		addStudent.setLayout(new GridLayout(5,1));
 		addStudent.add(IDpanel);
@@ -81,61 +85,63 @@ public class studentManagement extends JFrame implements ActionListener{
 		addStudent.add(DeptPanel);
 		addStudent.add(PhonePanel);
 
-		insertInfo = new Button("ADD");
-		addStudent.add(insertInfo);
+		insertInfoBtn = new Button("ADD");
+		addStudent.add(insertInfoBtn);
 
-		////////////////////////////////////
-
+		/*
+		 *  DELETE STUDENT IN HERE BY SEARCHING ID
+	     */
 		deleteStudent.setLayout(new BorderLayout(5, 5));
 		IDpanel2 = new Panel();
 		stID2 = new Label("Student ID");
-		//put data here to delete
-		valID2 = new TextField(20);
-		search = new Button("SEARCH");
+		
+		searchIdToDelete = new TextField(20); //put id here to delete
+		searchToDeleteBtn = new Button("SEARCH"); 
 		IDpanel2.add(stID2);
-		IDpanel2.add(valID2);
-		IDpanel2.add(search);
+		IDpanel2.add(searchIdToDelete);
+		IDpanel2.add(searchToDeleteBtn);
 
-		inform = new TextField("please search ID");	//please search ID
-
+		informToDelete = new TextField("Please, Search Id to delete");	
 		deleteBtn = new Button("DELETE");
 		deleteBtn.setSize(300,10);
 		deleteStudent.add(IDpanel2,BorderLayout.NORTH);
-		deleteStudent.add(inform, BorderLayout.CENTER);
+		deleteStudent.add(informToDelete, BorderLayout.CENTER);
 		deleteStudent.add(deleteBtn, BorderLayout.SOUTH);
 
-		/////////////////////////////////////////
-
+		/*
+		 *  UPDATE STUDENT PHONENUMBER IN HERE BY SEARCHING ID 
+	     */
 		updateStudent.setLayout(new BorderLayout(5, 5));
 		IDpanel3 = new Panel();
 		stID3 = new Label("Student ID");
-		valID3 = new TextField(20);
-		search2 = new Button("SEARCH");
+		searchIdToUpdate = new TextField(20); //id to update 
+		searchToUpdateBtn = new Button("SEARCH");
 		IDpanel3.add(stID3);
-		IDpanel3.add(valID3);
-		IDpanel3.add(search2);
+		IDpanel3.add(searchIdToUpdate);
+		IDpanel3.add(searchToUpdateBtn);
 
-		inform2 = new TextField("please search ID");	//please search ID
+		informToUpdate = new TextField("Please, Search Id to Update Phone Number");	
 		phonePanel2 = new Panel();
 		newPhone = new Label("New Phone Number");
-		valNewPhone = new TextField(20);
+		updatedPhone = new TextField(20); //phone number to update
 		updateBtn = new Button("UPDATE");
 		phonePanel2.add(newPhone);
-		phonePanel2.add(valNewPhone);
+		phonePanel2.add(updatedPhone);
 		phonePanel2.add(updateBtn);
 
 		updateStudent.add(IDpanel3,BorderLayout.NORTH);
-		updateStudent.add(inform2,BorderLayout.CENTER);
+		updateStudent.add(informToUpdate,BorderLayout.CENTER);
 		updateStudent.add(phonePanel2,BorderLayout.SOUTH);
 
-		////////////////////////////////////////
-
+		/*
+		 *  VIEW STUDENT INFORMATION IN HERE BY SEARCHING ID 
+	     */
 		JPanel searchInfo = new JPanel();
 		stID4 = new Label("Student ID");
-		valID4 = new TextField(20);
+		searchIdToView = new TextField(20); // id to searchToDeleteBtn 
 		viewBtn = new Button("SEARCH");
 		searchInfo.add(stID4);
-		searchInfo.add(valID4);
+		searchInfo.add(searchIdToView);
 		searchInfo.add(viewBtn);
 
 
@@ -143,22 +149,21 @@ public class studentManagement extends JFrame implements ActionListener{
 		scroll.setSize(17,20);
 		viewStudent.add(searchInfo,BorderLayout.NORTH);
 		viewStudent.add(scroll,BorderLayout.SOUTH);
-		////////////////////////////////////////////////
-
+		
 		addStudent.setBackground(Color.white);
-		deleteStudent.setBackground(Color.pink);
+		deleteStudent.setBackground(Color.white);
 		updateStudent.setBackground(Color.white);
-		viewStudent.setBackground(Color.pink);
+		viewStudent.setBackground(Color.white);
 
 		tabpane.addTab("Add New Student", addStudent);
 		tabpane.addTab("Delete Student", deleteStudent);
 		tabpane.addTab("Update Student's Phone Number", updateStudent);
 		tabpane.addTab("View Student's Information", viewStudent);
 
-		insertInfo.addActionListener(this);
+		insertInfoBtn.addActionListener(this);
 		deleteBtn.addActionListener(this);
-		search.addActionListener(this);
-		search2.addActionListener(this);
+		searchToDeleteBtn.addActionListener(this);
+		searchToUpdateBtn.addActionListener(this);
 		updateBtn.addActionListener(this);
 		viewBtn.addActionListener(this);
 
@@ -166,27 +171,27 @@ public class studentManagement extends JFrame implements ActionListener{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(700,400);
 		setVisible(true);
-		//check db connected and toast message
-		dbconnectionCheck(); 
-
+		
+		dbconnectionCheck();  //check db connected and toast message
+		
 	}
 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		//Event Perform
-		if(e.getSource() == insertInfo){
+		if(e.getSource() == insertInfoBtn){
 			addStudentInfo();
 		}else if(e.getSource()== deleteBtn){
 			deleteStudentById();
 		}else if(e.getSource()== viewBtn){
-			viewStudentById(valID4.getText().trim());
-		}else if(e.getSource()== search){
-			searchId(valID2.getText().trim(),inform," exists, you can delete!");
-		}else if(e.getSource()== search2){
-			searchId(valID3.getText().trim(),inform2," exists, please update phone number!");
+			viewStudentById(searchIdToView.getText().trim());
+		}else if(e.getSource()== searchToDeleteBtn){
+			searchId(searchIdToDelete.getText().trim(),informToDelete," exists, you can delete!");
+		}else if(e.getSource()== searchToUpdateBtn){
+			searchId(searchIdToUpdate.getText().trim(),informToUpdate," exists, please update phone number!");
 		}else if(e.getSource()== updateBtn){
-			updateStudentById(valID3.getText().trim(),valNewPhone.getText().trim());
+			updateStudentById(searchIdToUpdate.getText().trim(),updatedPhone.getText().trim());
 		}
 
 	}
@@ -217,29 +222,22 @@ public class studentManagement extends JFrame implements ActionListener{
 
 	void addStudentInfo(){
 		try{
-			String id = valID.getText().trim();
-			String name = valName.getText().trim();
-			String department = valDept.getText().trim();
-			String phone = valPhone.getText().trim();
-			if( id == null || name == null || department == null || valPhone == null ||
+			String id = insertId.getText().trim();
+			String name = insertName.getText().trim();
+			String department = insertDepartment.getText().trim();
+			String phone = insertPhone.getText().trim();
+				
+			if( id == null || name == null || department == null || insertPhone == null ||
 					id.length() == 0 || name.length() == 0 ||department.length() == 0 || phone.length() == 0)
 				return ;
-			//데이터베이스에 동일한 사용자ID가 있는지 확인
-			ResultSet rs2=stat.executeQuery("select id from student where id="+id);
-			if(rs2.next()) { 	
-				//valID.setText(id);
-				JOptionPane.showConfirmDialog(addStudent, "id", "Title", JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null);
-			}
 
 			String sql = "insert into student values(?,?,?,?)";
-			//Statement의 메소드를 이용해서 SQL문의 실행
 			PreparedStatement stat=conn.prepareStatement(sql);
 			stat.setString(1, id);
 			stat.setString(2, name);
 			stat.setString(3, department );
 			stat.setString(4, phone);
 			stat.executeUpdate();
-			//clear();
 			System.out.println("student data insert success");
 		}catch(Exception ex){
 
@@ -250,24 +248,16 @@ public class studentManagement extends JFrame implements ActionListener{
 	void deleteStudentById(){
 
 		try{
-			String id = valID2.getText().trim();
-			//삭제할 id 잘 받아왔는지 확인 
-			//System.out.println(valID2);
+			String id = searchIdToDelete.getText().trim();
 			if(id == null || id.length() == 0)
 				return ;
-			//데이터베이스에 동일한 사용자ID가 있는지 확인
-			ResultSet rs2=stat.executeQuery("select id from student where id="+id);
-			if(rs2.next()) { 	
-				//이름.setText("이미 등록되어있는 학생 ID 입니다.");
-			}
+			
 			//data delete query
 			stat.executeUpdate("delete from student where id=' " + id + " ' ");
-
 			System.out.println("student data delete success ");	
 		}catch(Exception e1){
 
 		}
-
 	}
 
 	void viewStudentById(String id){
